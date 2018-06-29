986c4f042d425c2e60bb95779e6d8cdebb2d4aea
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-06-29 16:16:32 +0300

    0.0.88

--- decorators/inject-soft/inject-soft.decorator.js
+++ decorators/inject-soft/inject-soft.decorator.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const container_1 = require("../../container");
 const module_service_1 = require("../../services/module/module.service");
 function InjectSoft(Service) {
-    return container_1.Container.get(module_service_1.ModuleService).watcherService.getByClass(Service)['value'];
+    return container_1.Container.get(module_service_1.ModuleService).watcherService.getByClass(Service);
 }
 exports.InjectSoft = InjectSoft;
--- src/decorators/inject-soft/inject-soft.decorator.ts
+++ src/decorators/inject-soft/inject-soft.decorator.ts
@@ -2,5 +2,5 @@ import { Container } from "../../container";
 import { ModuleService } from "../../services/module/module.service";
 
 export function InjectSoft<T>(Service: Function): T {
-    return Container.get(ModuleService).watcherService.getByClass(<any>Service)['value'];
+    return Container.get(ModuleService).watcherService.getByClass(<any>Service);
 }
