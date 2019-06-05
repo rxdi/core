9b06e4be74a2d6334c46e8b2b7ffd9c10922d9b6
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-06-05 20:28:12 +0300

    fix(InjectMany): client side added check to lazy evaluate Container.get

--- dist/container/decorators/InjectMany.js
+++ dist/container/decorators/InjectMany.js
@@ -3,33 +3,41 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Container_1 = require("../Container");
 const Token_1 = require("../Token");
 const CannotInjectError_1 = require("../error/CannotInjectError");
+const isClient = () => typeof module !== 'undefined' && module.exports && module['hot'];
 /**
  * Injects a service into a class property or constructor parameter.
  */
 function InjectMany(typeOrName) {
     return function (target, propertyName, index) {
+        if (isClient() && typeOrName instanceof Token_1.Token) {
+            Object.defineProperty(target, propertyName, {
+                get: () => prepareToken(Container_1.Container, target, propertyName, typeOrName)
+            });
+            return;
+        }
         if (!typeOrName)
             typeOrName = () => Reflect.getMetadata('design:type', target, propertyName);
         Container_1.Container.registerHandler({
             object: target,
             propertyName: propertyName,
             index: index,
-            value: containerInstance => {
-                let identifier;
-                if (typeof typeOrName === 'string') {
-                    identifier = typeOrName;
-                }
-                else if (typeOrName instanceof Token_1.Token) {
-                    identifier = typeOrName;
-                }
-                else {
-                    identifier = typeOrName();
-                }
-                if (identifier === Object)
-                    throw new CannotInjectError_1.CannotInjectError(target, propertyName);
-                return containerInstance.getMany(identifier);
-            }
+            value: containerInstance => prepareToken(containerInstance, target, propertyName, typeOrName)
         });
     };
 }
 exports.InjectMany = InjectMany;
+function prepareToken(containerInstance, target, propertyName, typeOrName) {
+    let identifier;
+    if (typeof typeOrName === 'string') {
+        identifier = typeOrName;
+    }
+    else if (typeOrName instanceof Token_1.Token) {
+        identifier = typeOrName;
+    }
+    else {
+        identifier = typeOrName();
+    }
+    if (identifier === Object)
+        throw new CannotInjectError_1.CannotInjectError(target, propertyName);
+    return containerInstance.getMany(identifier);
+}
--- src/container/decorators/InjectMany.ts
+++ src/container/decorators/InjectMany.ts
@@ -1,6 +1,8 @@
 import { Container } from '../Container';
 import { Token } from '../Token';
 import { CannotInjectError } from '../error/CannotInjectError';
+import { ContainerInstance } from 'container/ContainerInstance';
+const isClient = () => typeof module !== 'undefined' && module.exports && module['hot'];
 
 /**
  * Injects a service into a class property or constructor parameter.
@@ -20,33 +22,45 @@ export function InjectMany(token: Token<any>): Function;
 /**
  * Injects a service into a class property or constructor parameter.
  */
-export function InjectMany(typeOrName?: ((type?: any) => Function) | string | Token<any>): Function {
-    return function (target: Object, propertyName: string, index?: number) {
-
-        if (!typeOrName)
-            typeOrName = () => (Reflect as any).getMetadata('design:type', target, propertyName);
-
-        Container.registerHandler({
-            object: target,
-            propertyName: propertyName,
-            index: index,
-            value: containerInstance => {
-                let identifier: any;
-                if (typeof typeOrName === 'string') {
-                    identifier = typeOrName;
-
-                } else if (typeOrName instanceof Token) {
-                    identifier = typeOrName;
-
-                } else {
-                    identifier = typeOrName();
-                }
-
-                if (identifier === Object)
-                    throw new CannotInjectError(target, propertyName);
-
-                return containerInstance.getMany<any>(identifier);
-            }
+export function InjectMany(
+  typeOrName?: ((type?: any) => Function) | string | Token<any>
+): Function {
+  return function(target: Object, propertyName: string, index?: number) {
+    if (isClient() && typeOrName instanceof Token) {
+        Object.defineProperty(target, propertyName, {
+            get: () => prepareToken((Container as any) as ContainerInstance, target, propertyName, typeOrName)
         });
-    };
+        return;
+    }
+    if (!typeOrName)
+      typeOrName = () =>
+        (Reflect as any).getMetadata('design:type', target, propertyName);
+
+    Container.registerHandler({
+      object: target,
+      propertyName: propertyName,
+      index: index,
+      value: containerInstance => prepareToken(containerInstance, target, propertyName, typeOrName)
+    });
+  };
+}
+
+function prepareToken(
+  containerInstance: ContainerInstance,
+  target: Object,
+  propertyName: string,
+  typeOrName: ((type?: any) => Function) | string | Token<any>
+) {
+  let identifier: any;
+  if (typeof typeOrName === 'string') {
+    identifier = typeOrName;
+  } else if (typeOrName instanceof Token) {
+    identifier = typeOrName;
+  } else {
+    identifier = typeOrName();
+  }
+
+  if (identifier === Object) throw new CannotInjectError(target, propertyName);
+
+  return containerInstance.getMany<any>(identifier);
 }
